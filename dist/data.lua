4f48a6c7961b2087a73c7d64fd425e6030a83685
billbo99 <cactus_computers-github@yahoo.com>
2020-08-28 19:29:22 +0100

    1.0.0

--- dist/data.lua
+++ dist/data.lua
@@ -4,7 +4,7 @@ data:extend(
             type = "selection-tool",
             name = "chunk-eraser",
             icon = "__DeleteChunks__/graphics/chunk-eraser.png",
-            flags = {"hidden", "not-stackable"},
+            flags = {},
             subgroup = "tool",
             order = "c[automated-construction]-b[deconstruction-planner]",
             stack_size = 1,
@@ -17,6 +17,35 @@ data:extend(
             selection_cursor_box_type = "pair",
             alt_selection_cursor_box_type = "pair",
             show_in_library = true
+        },
+        {
+            type = "shortcut",
+            name = "chunk-eraser",
+            order = "o[chunk-eraser]",
+            action = "create-blueprint-item",
+            item_to_create = "chunk-eraser",
+            toggleable = true,
+            icon = {
+                filename = "__DeleteChunks__/graphics/chunk-eraser.png",
+                -- priority = "extra-high-no-scale",
+                size = 64,
+                scale = 0.5,
+                flags = {"gui-icon"}
+            },
+            small_icon = {
+                filename = "__DeleteChunks__/graphics/chunk-eraser.png",
+                -- priority = "extra-high-no-scale",
+                size = 64,
+                scale = 0.5,
+                flags = {"gui-icon"}
+            },
+            disabled_small_icon = {
+                filename = "__DeleteChunks__/graphics/chunk-eraser.png",
+                -- priority = "extra-high-no-scale",
+                size = 64,
+                scale = 0.5,
+                flags = {"gui-icon"}
+            }
         }
     }
 )
